7cdba1742c5e42aafe29cb4931347a5bd275790e
Abdulrahman-A-Rashad <abdulrahmanrashad9595@gmail.com>
2025-03-03 22:48:51 +0200

    final uptade for stable release

--- Keil/define_ports.h
+++ Keil/define_ports.h
@@ -1,7 +1,7 @@
 sbit EN = P2^1; // Enable LCD
-sbit RW = P2^3; // Read Write 
+sbit RW = P2^2; // Read Write 
 sbit RS = P2^0; // Register Select
-sbit signal_pin = P3^4;  // External signal input
+sbit signal_pin = P3^5;  // External signal input
 
 #define LCD_PORT P1
 int i, j;
--- Keil/main.c
+++ Keil/main.c
@@ -4,53 +4,55 @@
 #include "lcd.h"
 #include "results.h"
 
-
-
 unsigned int pulse_count = 0;
 unsigned int frequency = 0;
-
+unsigned int last_frequency = 0;
 void timer0_isr() interrupt 1 
 	{
-    TR0 = 0; 
-    TR1 = 0;  
+    TR0 = 0;  // Stop Timer 0
+    TR1 = 0;  // Stop Timer 1
 
-    pulse_count = TH1;  
-    pulse_count = (pulse_count << 8) | TL1;
+    // Read Timer 1 count
+    pulse_count = (TH1 << 8) | TL1;
     
-    frequency = pulse_count * 20; 
+    // Frequency is directly the counted pulses * 20 
+    frequency = pulse_count*20;
 
+    // Reset Timer 0 for 50ms delay
     TH0 = 0x3C;
     TL0 = 0xB0;
-    
-    TH1 = 0;  
-    TL1 = 0;
 
-    TR0 = 1;  
-    TR1 = 1;  
+    // Reset Timer 1 for new measurement
+    TH1 = 0x00;
+    TL1 = 0x00;
+
+    TR0 = 1;  // Start Timer 0
+    TR1 = 1;  // Start Timer 1
   }
 
 void setup_timers() 
 	{
-    TMOD = 0x51;  
-    
-    TH0 = 0x4B;  
-    TL0 = 0xFD;
+    TMOD = 0x51;  // Timer 0:(16-bit timer), Timer 1:(16-bit counter)
 
-    TH1 = 0;  
-    TL1 = 0;
-    
-    ET0 = 1;  
-    EA = 1;   
+    // Timer 0 for 50ms
+    TH0 = 0x3C;
+    TL0 = 0xB0;
 
-    TR0 = 1;  
-    TR1 = 1; 
+    TH1 = 0x00;  // Clear Timer 1 counter
+    TL1 = 0x00;
+
+    ET0 = 1;  // Enable Timer 0 Interrupt & global interrupts
+    EA = 1; 
+
+    TR0 = 1;  // Start Timer 0 & Timer 1
+    TR1 = 1;  
   }
 
-void display_frequency(unsigned int freq)
+void display_frequency(unsigned int freq) 
 	{ 
     lcd_print("Freq: ");
-		print_result(freq);
-		
+    print_result(freq);
+	  lcd_print("Hz");
   }
 
 void main() 
@@ -59,8 +61,11 @@ void main()
     setup_timers();
 
     while (1) 
-			{
-        display_frequency(frequency);
-        delay(100);  // Small delay before updating LCD
+			{ 
+				if (frequency != last_frequency) // To avoid keep uptading the LCD
+				{
+						display_frequency(frequency);
+        }
+				last_frequency = frequency; 
       }
   }
