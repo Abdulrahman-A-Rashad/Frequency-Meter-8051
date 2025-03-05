146d98be5ba67373e7d8bc11069e5f8dcb8bff0c
Sherif Dahy <137155173+sherifdahy@users.noreply.github.com>
2025-03-05 17:10:07 +0200

    Delete Keil directory

--- Keil/README.md
+++
@@ -1,2 +0,0 @@
-# Frequency-Meter-8051
-Calculating Frequency for a given signal using 8051 micro controller
--- Keil/define_ports.h
+++
@@ -1,8 +0,0 @@
-sbit EN = P2^1; // Enable LCD
-sbit RW = P2^2; // Read Write 
-sbit RS = P2^0; // Register Select
-sbit signal_pin = P3^5;  // External signal input
-
-#define LCD_PORT P1
-int i, j;
-unsigned char x,y;
